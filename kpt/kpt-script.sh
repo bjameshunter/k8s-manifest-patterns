f2faa2dfc16d9f98d86d7193a21a990723a6e3c4
Benjamin Hunter <benjamin.hunter@endava.com>
2022-10-06 17:28:19 -0500

    external-dns and some kpt

--- kpt/kpt-script.sh
+++ kpt/kpt-script.sh
@@ -3,12 +3,12 @@
 K8S_ENV=dev
 # kpt fn eval -i list-setters:v0.1.0
 
-# kpt fn eval -i apply-setters:v0.2.0 -- \
-#     environment=dev \
-#     nginx-replicas=2 \
-#     namespace=kpt-dev \
-#     app-name=web
-#
+kpt fn eval -i apply-setters:v0.2.0 -- \
+    environment=$K8S_ENV \
+    nginx-replicas=2 \
+    tech=kpt \
+    app-name=web
+
 kpt fn eval -i set-namespace:v0.4.0 -- \
-    namespace=kpt-$K8S_ENV
+    namespace=$K8S_ENV-kpt
 
